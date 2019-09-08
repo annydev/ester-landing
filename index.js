fcb79c6a867fceae5c1a2da4fcd80f7987bbfa6d
Anny <dandara.anna13@hotmail.com>
2019-09-08 15:38:30 +0300

    finished slider for parfums

--- index.js
+++ index.js
@@ -3,28 +3,59 @@ $(".next-button").click(function() {
   var closestClassCatalogSlider = selectedButton.closest(".catalog-sliders");
 
   closestClassCatalogSlider.addClass("d-none");
-  closestClassCatalogSlider.removeClass("d-flex");
 
   var nextCatalogSliderElement = closestClassCatalogSlider.next();
 
 
   if (nextCatalogSliderElement.length === 0) {
-    nextCatalogSliderElement = $(".catalog-slider").first();
+    nextCatalogSliderElement = $(".catalog-sliders").first();
   }
 
-  nextCatalogSliderElement.addClass("d-flex");
   nextCatalogSliderElement.removeClass("d-none");
 });
+
+$(".previous-button").click(function() {
+  var chosenButton = $(this);
+  var closestClassCatalogSlider = chosenButton.closest(".catalog-sliders");
+
+  closestClassCatalogSlider.addClass("d-none");
+
+  var previousCatalogSliderElement = closestClassCatalogSlider.prev();
+
+  if (previousCatalogSliderElement.length === 0) {
+    previousCatalogSliderElement = $(".catalog-sliders").last();
+  }
+
+  previousCatalogSliderElement.removeClass("d-none");
+});
+
+$(".man").click(function() {
+  var clickedButton = $(this);
+  $(".woman").removeClass("active");
+  $(".man").addClass("active");
+  $(".women-parfums").hide();
+  $(".men-parfums").show();
+});
+
+$(".woman").click(function() {
+  var clickedButton = $(this);
+  $(".man").removeClass("active");
+  $(".woman").addClass("active");
+  $(".women-parfums").show();
+  $(".men-parfums").hide();
+});
+
 
 $(document).scroll(function() {
   var $nav = $("#navigation-bar");
   $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
 });
 
-$('#modal-1').on('show.bs.modal', function (event) {
-  var button = $(event.relatedTarget); // Button that triggered the modal
-  var recipient = button.data('whatever');
-  var modal = $(this);
-  modal.find('.modal-title').text("Какой аромат хотите заказать?");
-  modal.find('.modal-body input').val(recipient);
+$(".mode-footer-button").click(function() {
+  if ($(".mode-phone").val() === "") {
+    $(".if-statement").removeClass("d-none");
+  } else {
+    $('#orderParfumeModal').modal('hide');
+    $('#afterOrder').modal('show');
+  }
 });
