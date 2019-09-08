0a58669baaea7effe94014e86bef83f69b9fbc26
Anny <dandara.anna13@hotmail.com>
2019-09-08 23:48:57 +0300

    added map section

--- index.js
+++ index.js
@@ -33,16 +33,16 @@ $(".man").click(function() {
   var clickedButton = $(this);
   $(".woman").removeClass("active");
   $(".man").addClass("active");
-  $(".women-parfums").hide();
-  $(".men-parfums").show();
+  $(".women-parfums").addClass("d-none");
+  $(".men-parfums").removeClass("d-none");
 });
 
 $(".woman").click(function() {
   var clickedButton = $(this);
   $(".man").removeClass("active");
   $(".woman").addClass("active");
-  $(".women-parfums").show();
-  $(".men-parfums").hide();
+  $(".women-parfums").removeClass("d-none");
+  $(".men-parfums").addClass("d-none");
 });
 
 
@@ -52,10 +52,65 @@ $(document).scroll(function() {
 });
 
 $(".mode-footer-button").click(function() {
-  if ($(".mode-phone").val() === "") {
-    $(".if-statement").removeClass("d-none");
+  var chosenButton = $(this);
+  var currentModal = chosenButton.closest(".modal");
+
+  if (currentModal.find(".mode-phone").val() === "") {
+    currentModal.find(".if-statement").removeClass("d-none");
   } else {
-    $('#orderParfumeModal').modal('hide');
+    currentModal.modal('hide');
     $('#afterOrder').modal('show');
   }
+});
+
+$('.modal').on('hidden.bs.modal', function(e) {
+  var currentModal = $(this);
+
+  currentModal.find("input").val("");
+  currentModal.find("select").val("");
+  currentModal.find(".if-statement").addClass("d-none");
+});
+
+$(".catalog-slider-button").click(function() {
+  var currentButton = $(this);
+  var parfumName = currentButton.attr("name");
+
+  var chosenModal = $("#orderParfumeModal");
+  var parfumSelect = chosenModal.find(".select-parfums");
+
+  parfumSelect.val(parfumName);
+  chosenModal.modal('show');
+
+});
+
+$(".previous-certificate-button").click(function() {
+  var activeImage = $(".certificate-images .active");
+
+  activeImage.addClass("d-none");
+  activeImage.removeClass("active");
+
+  var previousImage = activeImage.prev();
+
+  if (previousImage.length === 0) {
+    previousImage = $(".certificate-images img").last();
+  }
+
+  previousImage.removeClass("d-none");
+  previousImage.addClass("active");
+});
+
+$(".next-certificate-button").click(function() {
+  var activeImage = $(".certificate-images .active");
+
+  activeImage.addClass("d-none");
+  activeImage.removeClass("active");
+
+  var nextImage = activeImage.next();
+
+  if (nextImage.length === 0) {
+    nextImage = $(".certificate-images img").first();
+  }
+
+  nextImage.removeClass("d-none");
+  nextImage.addClass("active");
 });
